0f7599e3c80439d11213a3d3c8055dcd1c176f29
Fer <f.clerkx@student.fontys.nl>
2024-10-28 15:45:00 +0100

    Renamed program_20 -> freq_mag_response. Improve ability to handle non-ideal inputs and now accepts complete pole/zero pairs.

--- freq_mag_response.m
+++ freq_mag_response.m
@@ -1,19 +1,25 @@
-function [h_abs, k, w] = program_20(p, z)
+function [h_abs, k, w] = freq_mag_response(p, z)
     w = linspace(0,pi,320);
 
     for i=numel(p):-1:1
+        if abs(imag(p(i))) < 1E-3
+            p(i) = real(p(i));
+        end
         if isreal(p(i))
             Fp_abs(i,:) = sqrt(1 - 2*p(i)*cos(w) + p(i)^2);
-        else
+        elseif imag(p(i)) > 1E-3  % Skip negative part of complex pole pair
             r = abs(p(i));
             theta = angle(p(i));
             Fp_abs(i,:) = sqrt((cos(2*w) - 2*r*cos(theta)*cos(w) + r^2).^2 + (sin(2*w) - 2*r*cos(theta)*sin(w)).^2);
         end
     end
     for i=numel(z):-1:1
+        if abs(imag(z(i))) < 1E-3
+            z(i) = real(z(i));
+        end
         if isreal(z(i))
             Fz_abs(i,:) = sqrt(1 - 2*z(i)*cos(w) + z(i)^2);
-        else
+        elseif imag(z(i)) > 1E-3  % Skip negative part of complex zero pair
             r = abs(z(i));
             theta = angle(z(i));
             Fz_abs(i,:) = sqrt((cos(2*w) - 2*r*cos(theta)*cos(w) + r^2).^2 + (sin(2*w) - 2*r*cos(theta)*sin(w)).^2);
