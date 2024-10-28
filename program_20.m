559eeaa7ad6e6ea55b59b49951dfbb8dd124b0a9
Fer <f.clerkx@student.fontys.nl>
2024-10-28 14:50:58 +0100

    Added .gitignore, refactored program_20 into function

--- program_20.m
+++ program_20.m
@@ -1,35 +1,34 @@
-clear
+function [h_abs, k, w] = program_20(p, z)
+    w = linspace(0,pi,320);
 
-p = [0.9738*exp(pi/10*1j)];
-z = [exp(pi/10*1j)];
+    for i=numel(p):-1:1
+        if isreal(p(i))
+            Fp_abs(i,:) = sqrt(1 - 2*p(i)*cos(w) + p(i)^2);
+        else
+            r = abs(p(i));
+            theta = angle(p(i));
+            Fp_abs(i,:) = sqrt((cos(2*w) - 2*r*cos(theta)*cos(w) + r^2).^2 + (sin(2*w) - 2*r*cos(theta)*sin(w)).^2);
+        end
+    end
+    for i=numel(z):-1:1
+        if isreal(z(i))
+            Fz_abs(i,:) = sqrt(1 - 2*z(i)*cos(w) + z(i)^2);
+        else
+            r = abs(z(i));
+            theta = angle(z(i));
+            Fz_abs(i,:) = sqrt((cos(2*w) - 2*r*cos(theta)*cos(w) + r^2).^2 + (sin(2*w) - 2*r*cos(theta)*sin(w)).^2);
+        end
+    end
 
-omega = linspace(0,pi,320);
+    h_abs = log10(prod(Fz_abs,1)./prod(Fp_abs,1))*20;
+    k = max(h_abs);
+    h_abs = h_abs - k;
 
-for i=numel(p):-1:1
-    if isreal(p(i))
-        Fp_abs(i,:) = sqrt(1 - 2*p(i)*cos(omega) + p(i)^2);
-    else
-        r = abs(p(i));
-        theta = angle(p(i));
-        Fp_abs(i,:) = sqrt((cos(2*omega) - 2*r*cos(theta)*cos(omega) + r^2).^2 + (sin(2*omega) - 2*r*cos(theta)*sin(omega)).^2);
+    if nargout ~= 3
+        plot(w/pi, h_abs)
+        xlabel("Normalized Frequency (pi*rad/sample)")
+        ylabel("Magnitude (dB)")
+        title("Peak gain = " + k + "dB")
+        grid on
     end
 end
-for i=numel(z):-1:1
-    if isreal(z(i))
-        Fz_abs(i,:) = sqrt(1 - 2*z(i)*cos(omega) + z(i)^2);
-    else
-        r = abs(z(i));
-        theta = angle(z(i));
-        Fz_abs(i,:) = sqrt((cos(2*omega) - 2*r*cos(theta)*cos(omega) + r^2).^2 + (sin(2*omega) - 2*r*cos(theta)*sin(omega)).^2);
-    end
-end
-
-H_abs = log10(prod(Fz_abs,1)./prod(Fp_abs,1))*20;
-peak_gain = max(H_abs);
-H_abs = H_abs - peak_gain;
-
-plot(omega/pi, H_abs)
-xlabel("Normalized Frequency (pi*rad/sample)")
-ylabel("Magnitude (dB)")
-title("Peak gain = " + peak_gain + "dB")
-grid on
